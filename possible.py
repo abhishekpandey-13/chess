580750821ef1083a30008fcb18022b42b18ac9b2
Abhishek Pandey <abhishekpandey_97@yahoo.com>
2017-04-26 23:45:52 +0530

    movement control

---
+++ possible.py
@@ -0,0 +1,38 @@
+import math
+#haathi
+def possible_haathi(current,end):
+    if(((end-current)%8==0) or ((current-end)%8==0) or ((math.floor(current/8)*8)<end<=(math.ceil(current/8)*8))):
+        return "true"
+    else:
+        return "false"
+#pyada
+def possible_pyada(current,end):
+    if((end-current)==8 or (current-end)==8):
+        print("true")
+    else:
+        print("false")
+#oontth
+def possible_oontth(current,end):
+    if((end-current)%9==0 or (end-current)%7==0):
+        return "true"
+    else:
+        return "false"
+#ghoda
+def possible_ghoda(current,end):
+    if(abs(end-current) in [6,10,15,17]):
+        print("true")
+    else:
+        print("false")
+#raja
+def possible_raja(current,end):
+    if(abs(end-current) in [1,7,8,9]):
+        print("true")
+    else:
+        print("false")
+
+def possible_rani(current,end):
+    if((possible_oontth(current,end) == 'true') or (possible_haathi(current,end) == 'true')):
+        print('true')
+    else:
+        print('False')
+possible_rani(57,1)
